6c5770b2f2767ec17beac9daca2996ab608d7ff1
Sidney <sidneynguyendev@gmail.com>
2016-11-10 19:56:28 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <title>Algae</title>
+</head>
+
+<body>
+    <header>
+        <h1 id="title">Algae</h1>
+        
+        <img id="algae_pic" src="algae.png" alt="Algae" />
+    </header>
+
+    <main class="content">
+        <section class="data" id="temp">
+            <h3>Temperature</h3>
+            <form action="temp.html">
+                <input type="submit" value="temperature" />
+            </form>
+        </section>
+
+        <section class="data" id="humid">
+            <h3>Humidity</h3>
+            <form action="humid.html">
+                <input type="submit" value="Humidity" />
+            </form>
+        </section>
+
+        <section class="data" id="light">
+            <h3>Light Sensitivity</h3>
+            <form action="light.html">
+                <input type="submit" value="Light Sesitivity" />
+            </form>
+        </section>
+    </main>
+</body>
+
+
+</html>
